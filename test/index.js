65eb17fe79d4009393d9148ba9df38d080a40213
Alexander Schilling <info@alinex.de>
2014-03-27 21:16:55 +0100

    Added simple test script.

---
+++ test/index.js
@@ -0,0 +1,12 @@
+/**
+* Test script
+* =================================================*/
+
+// include base modules
+errorHandler = require('../lib');
+errorHandler.install()
+
+// Easy script
+// -------------------------------------------------
+console.log("start");
+throw new Error("Something went wrong");
