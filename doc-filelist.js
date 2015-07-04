b46ebd418d7c6522da6b4692eb1dc48946584b0e
alinex <info@alinex.de>
2015-07-04 13:11:38 +0200

    Updated documentation

--- doc-filelist.js
+++ doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"files":[".travis.yml","Changelog.md","README.md"],"dirs":{"src":{"files":["index.coffee"]}}};
+var tree={"files":[".travis.yml","Changelog.md","README.md"],"dirs":{"coverage":{"dirs":{"lcov-report":{"files":["base.css","index.html","prettify.css","prettify.js","sorter.js"],"dirs":{"src":{"files":["index.coffee.html","index.html"]}}}}},"src":{"files":["index.coffee"]}}};
